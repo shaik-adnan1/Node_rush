ff897301784645a94de449f5c83db7df9413de76
shaik-adnan1 <yousufwrk0@gmail.com>
2023-09-20 23:26:32 +0530

    creating a simple server using http createserver()

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 const fs = require("fs");
-// const http = require('http')
+const http = require("http");
 
-// Blocking synchronous
-/*
-const gitRReadme = fs.readFileSync("./README.md", "utf-8");
-console.log(gitRReadme);
+// // Blocking synchronous
+// /*
+// const gitRReadme = fs.readFileSync("./README.md", "utf-8");
+// console.log(gitRReadme);
 
-const textOut = `this is what we know about the current project ${gitRReadme}. \n Create on ${Date.now()}`;
-fs.writeFileSync("./testREADME.md", textOut);
-console.log("file written");
-*/
-// Non - blocking Asynchronous
-fs.readFile("./starttttt.md", "utf-8", (err, data1) => {
-  if (err) console.log("error!");
-  fs.readFile(`./${data1}`, "utf-8", (err, data2) => {
-    // console.log(data2);
-    fs.writeFile(`./output.md`, `${data1} \n ${data2}`, "utf-8", err => {
-      console.log("your file has been written");
-      fs.readFile(`./output.md`, "utf-8", (err, data) => {
-        console.log(data);
-      });
-    });
-  });
-  // console.log(data1);
+// const textOut = `this is what we know about the current project ${gitRReadme}. \n Create on ${Date.now()}`;
+// fs.writeFileSync("./testREADME.md", textOut);
+// console.log("file written");
+// */
+// // Non - blocking Asynchronous
+// fs.readFile("./starttttt.md", "utf-8", (err, data1) => {
+//   if (err) console.log("error!");
+//   fs.readFile(`./${data1}`, "utf-8", (err, data2) => {
+//     // console.log(data2);
+//     fs.writeFile(`./output.md`, `${data1} \n ${data2}`, "utf-8", err => {
+//       console.log("your file has been written");
+//       fs.readFile(`./output.md`, "utf-8", (err, data) => {
+//         console.log(data);
+//       });
+//     });
+//   });
+//   // console.log(data1);
+// });
+// console.log("reading file");
+
+///////////////////////////////////////////
+// server
+
+const server = http.createServer((req, res) => {
+  console.log(req)
+  res.end("hello from the server");
+  // console.log(req)
 });
-console.log("reading file");
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to requests on port 8000");
+}); // localhost default -- 127.0.0.1
+// server.listen(8001, "127.0.0.1", () => {
+//   console.log("listening to requests on port 8000");
+// }); // localhost default -- 127.0.0.1
