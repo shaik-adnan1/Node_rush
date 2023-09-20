372ccf7d9fb70735c247cd8800ea8f9a10d5a49b
shaik-adnan1 <yousufwrk0@gmail.com>
2023-09-20 23:47:29 +0530

    created a simple server usig http module and implemented routing with the same

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 // // Blocking synchronous
 // /*
@@ -11,13 +12,13 @@ const http = require("http");
 // console.log("file written");
 // */
 // // Non - blocking Asynchronous
-// fs.readFile("./starttttt.md", "utf-8", (err, data1) => {
+// fs.readFile("./txt/start.md", "utf-8", (err, data1) => {
 //   if (err) console.log("error!");
 //   fs.readFile(`./${data1}`, "utf-8", (err, data2) => {
 //     // console.log(data2);
-//     fs.writeFile(`./output.md`, `${data1} \n ${data2}`, "utf-8", err => {
+//     fs.writeFile(`./txt/output.md`, `${data1} \n ${data2}`, "utf-8", err => {
 //       console.log("your file has been written");
-//       fs.readFile(`./output.md`, "utf-8", (err, data) => {
+//       fs.readFile(`./txt/output.md`, "utf-8", (err, data) => {
 //         console.log(data);
 //       });
 //     });
@@ -30,14 +31,24 @@ const http = require("http");
 // server
 
 const server = http.createServer((req, res) => {
-  console.log(req)
-  res.end("hello from the server");
+  console.log(req.url);
+
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the overview section");
+  } else if (pathName === "/products") {
+    res.end("this is the product section");
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html'
+    });
+    res.end("<h1>page not found</h1>");
+  }
+
   // console.log(req)
 });
 
 server.listen(8000, "127.0.0.1", () => {
   console.log("listening to requests on port 8000");
 }); // localhost default -- 127.0.0.1
-// server.listen(8001, "127.0.0.1", () => {
-//   console.log("listening to requests on port 8000");
-// }); // localhost default -- 127.0.0.1
