8204bd1513ff7bc18ca5e9d867b550f4023e77da
shaik-adnan1 <yousufwrk0@gmail.com>
2023-09-24 18:10:30 +0530

    done with the async part

--- asyncjs/index.js
+++ asyncjs/index.js
@@ -20,21 +20,61 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/doggg.txt`)
-  .then((data) => {
-    return superagnet.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
+
+    console.log(`Breed: ${data}`);
+
+    const res = await superagnet.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
     console.log(res.body.message);
 
-    writeFilePro('dog-img.txt', res.body.message)
-  })
-  .then(() => {
-    console.log(`random dog image save to file`)
-  })
-  .catch((err) => {
-    console.log(`This is the error ${err}`);
-  });
+    await writeFilePro('dog-img.txt', res.body.message);
+    console.log(`File is written`);
+  } catch (err) {
+    console.log(err);
+  }
+  return '2: Ready 🐶';
+};
+
+(async () => {
+  try {
+    console.log(`1: Will get dog pics`);
+    const x = await getDogPic();
+    console.log(`got the dog pic: ${x}`)
+
+    console.log(`3: Done getting dog pics`);
+  } catch (err) {
+    console.log('Error 💥');
+  }
+})();
+
+// console.log(`1: Will get dog pics`)
+// getDogPic().then(x => {
+//   console.log(x)
+//   console.log(`3: Done getting dog pics`);
+// }).catch(err => {
+//   console.log('Error 💥')
+// })
+
+// readFilePro(`${__dirname}/doggg.txt`)
+//   .then((data) => {
+//     return superagnet.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+
+//     writeFilePro('dog-img.txt', res.body.message)
+//   })
+//   .then(() => {
+//     console.log(`random dog image save to file`)
+//   })
+//   .catch((err) => {
+//     console.log(`This is the error ${err}`);
+//   });
 
 // fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => {
 //   console.log(data);
