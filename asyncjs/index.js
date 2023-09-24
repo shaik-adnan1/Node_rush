852f9a409f5dd306f53571d25e41193286d260ad
shaik-adnan1 <yousufwrk0@gmail.com>
2023-09-24 10:57:53 +0530

    Added readme

--- asyncjs/index.js
+++ asyncjs/index.js
@@ -8,5 +8,9 @@ fs.readFile(`${__dirname}/dog.txt`, 'utf-8', (err, data) => {
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
       console.log(res.body.message);
-    }); 
+
+      fs.writeFile('dog-img.txt', res.body.message, (err) => {
+        console.log('random dog image saved to file');
+      });
+    });
 });
